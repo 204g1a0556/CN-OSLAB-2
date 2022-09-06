89deab49bf43cd0452aca15738cc06dbfcd69bdf
204g1a0556 <94549018+204g1a0556@users.noreply.github.com>
2022-09-06 12:51:53 +0530

    V  MEGHANA Created SJF.C

---
+++ OSLAB/SJF.C
@@ -0,0 +1,7 @@
+ 
+/* Name Of the Candidate: V .MEGHANA
+   Roll No: 204G1A0556
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 6-09-2022
+   Date of Execution: 6-09-2022
+*/
