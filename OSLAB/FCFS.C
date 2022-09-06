e9f69f343215cf8c9b2d5e76682b7397b71d8b0f
204g1a0556 <94549018+204g1a0556@users.noreply.github.com>
2022-09-06 12:31:15 +0530

    V  MEGHANA  Created FCFS.C

---
+++ OSLAB/FCFS.C
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: V.MEGHANA
+   Roll No: 204G1A0556
+   Title of the Experiment: CPU Scheduling Using FCFS Algorithm
+   Date of Creation: 6-09-2022
+   Date of Execution: 6-09-2022
+*/
